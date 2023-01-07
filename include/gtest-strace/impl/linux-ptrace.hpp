5b5e2cda9faad084fe07cce62e8aa4619bd98307
doodspav <doodspav@gmail.com>
2023-01-07 21:10:49 +0000

    Implement function to create thread stack

--- include/gtest-strace/impl/linux-ptrace.hpp
+++ include/gtest-strace/impl/linux-ptrace.hpp
@@ -15,6 +15,7 @@
 #include <unistd.h>
 
 #include <algorithm>
+#include <cerrno>
 #include <cstddef>
 #include <exception>
 #include <csignal>
@@ -38,6 +39,21 @@ namespace linux_ptrace
         {
             (void) munmap(ptr, size);
         }
+    };
+
+
+    /// @brief Struct representing a thread's stack.
+    struct ThreadStack
+    {
+        /// @brief Whole allocation including guard pages.
+        std::unique_ptr<char, MUnmapDeleter> memory;
+
+        /// @brief Pointer to start of stack, not including guard pages.
+        /// @note  May be top or bottom, depending on which direction stack grows.
+        char *start;
+
+        /// @brief Size of stack in bytes, not including guard pages.
+        std::size_t size;
     };
 
 
@@ -102,6 +118,80 @@ namespace linux_ptrace
         {
             return std::max(limit.rlim_cur, min_size) + guard_pages;
         }
+    }
+
+
+    /// @brief   Checks that stack grows from high address to low address.
+    /// @returns true if stack grows down, false if it doesn't.
+    [[nodiscard, gnu::const]] inline constexpr bool
+    CheckStackGrowsDownwards() noexcept
+    {
+        // on linux only HP PA platforms have a stack that doesn't grow downwards
+#if defined(__hppa__) || defined(__HPPA__) || defined(__hppa)
+        return false;
+#else
+        return true;
+#endif
+
+    }
+
+
+    /// @brief  Create stack to be used by new process/thread.
+    /// @note   On failure (denoted by result_out param), return type will be zero-initialised.
+    [[nodiscard]] ThreadStack
+    CreateStack(AssertionResult *result_out) noexcept
+    {
+        // allocate memory for stack
+        std::size_t stack_size = GetInitialStackSize();
+        char *stack_ptr = reinterpret_cast<char *>(mmap(
+            nullptr, stack_size, PROT_NONE,
+            MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK,
+            -1, 0
+        ));
+
+        // check for failure
+        if (!stack_ptr)
+        {
+            *result_out = AssertionFailure()
+                << "Failed to mmap " << stack_size << " bytes for new stack"
+                << " with errno: " << errno;
+            return {};
+        }
+
+        // store in unique_ptr now, so that on failure we can return without
+        // needing to manually free the memory
+        MUnmapDeleter del {stack_size};
+        std::unique_ptr<char, MUnmapDeleter> uptr {stack_ptr, del};
+
+        // make non-guard page portions readable and writable
+        // add guard page at start of stack
+        stack_ptr  += GetPageSize();
+        stack_size -= GetPageSize();
+        // add guard page at end of stack
+        stack_size -= GetPageSize();
+        // change protections
+        int res = mprotect(
+            stack_ptr, stack_size,
+            PROT_READ | PROT_WRITE
+        );
+
+        // check for failure
+        if (res == -1)
+        {
+            *result_out = AssertionFailure()
+                << "Failed to change memory protections to READ | WRITE on"
+                << " memory region " << stack_ptr << " with size " << stack_size
+                << " with errno: " << errno;
+            return {};
+        }
+
+        // get start of stack (so high address if stack grows downwards)
+        constexpr bool flip = CheckStackGrowsDownwards();
+        stack_ptr += (flip * stack_size);
+
+        // success
+        *result_out = AssertionSuccess();
+        return {std::move(uptr), stack_ptr, stack_size};
     }
 
 
